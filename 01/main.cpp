19166a8e60c4be0eb24ce8a334b6f68bd371fad0
Chinmay0303 <99880235+Chinmay0303@users.noreply.github.com>
2026-08-14 12:06:56 +0530

    add comments and change screen color to red

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,17 +1,46 @@
 #include <iostream>
 #include <GL/freeglut.h>
-using namespace std;
+// using namespace std;
+using std::cout;
 
 static void RenderSceneCB(){
+
+    static int scene = 0;
+    // initialized only once, and is not destroyed when the function returns a value, it retains its value between function calls
+    // a function can also return the pointer to a static local variable, unlike normal variables which are destroyed when the function exits
+
+    static GLclampf red = 1.0f, green = 0.0f, blue = 0.0f, alpha = 0.0f;
+    // The color has four channels (RGBA) and it is specified as a normalized value between 0.0 and 1.0
+    glClearColor(red, green, blue, alpha); // colour buffer now stores this; default is all 0.0f
+
     glClear(GL_COLOR_BUFFER_BIT);
-    glutSwapBuffers();
+    // clear the frame buffer using the colour specified above
+
+    // glFlush(); // for only single buffer
+
+    if(scene % 100 == 0){
+        cout << "Render_Scene_" << scene << "\n";
+    }
+    scene += 1;
+
+    glutPostRedisplay(); // allows the RenderSceneCB function to be called continuously by glut
+
+    glutSwapBuffers(); // swaps the roles of front and back buffers, allowing for double buffering to prevent flickering
 }
 
 int main(int argc, char* argv[]){
 
     glutInit(&argc,argv);
-    glutInitDisplayMode(GLUT_DOUBLE| GLUT_RGBA | GLUT_DEPTH);
 
+    // bitmasking ??
+    // glutInitDisplayMode(GLUT_DEPTH);
+    // glutInitDisplayMode(GLUT_DOUBLE| GLUT_RGBA | GLUT_DEPTH);
+    // glutInitDisplayMode(GLUT_SINGLE| GLUT_RGBA);
+    glutInitDisplayMode(GLUT_DOUBLE| GLUT_RGBA);
+    // single frame buffer and double frame buffers(front and back)
+    // double buffer allows us to draw to a back buffer while the front buffer is being displayed
+    // colour buffer
+    // depth buffer
 
     int width = 600;
     int height = 300;
@@ -22,13 +51,13 @@ int main(int argc, char* argv[]){
     glutInitWindowSize(width, height);
     glutInitWindowPosition(x,y);
 
-    int window = glutCreateWindow("Tutorial 01");
+    int window = glutCreateWindow("Tutorial 01"); // returns an integer value and takes a string input
 
-    cout << "window_id = " << window << endl;
+    cout << "window_id = " << window << "\n";
 
-    glutDisplayFunc(RenderSceneCB);
+    glutDisplayFunc(RenderSceneCB); // event call back function, called continuously by glut internal loop
 
-    glutMainLoop();
+    glutMainLoop(); // begin glut internal loop
 
     return 0;
 
