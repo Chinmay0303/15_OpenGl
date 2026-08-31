13646ee4ba6af112ec84ddfbbdbf82c92ce7c701
Chinmay0303 <99880235+Chinmay0303@users.noreply.github.com>
2026-08-31 18:04:31 +0530

    move camera and use worldtransform, camera classes

---
+++ 23_Camera_Movement/build.sh
@@ -0,0 +1,7 @@
+source ../build_base.sh
+
+LDFLAGS=`pkg-config --libs glew`
+LDFLAGS="$LDFLAGS -lglut -lX11"
+
+$CC main.cpp ../Common/ogldev_util.cpp  ../Common/math_3d.cpp world_transform.cpp camera.cpp $OGL_CPPFLAGS $LDFLAGS -o main
+
---
+++ 23_Camera_Movement/camera.h
@@ -0,0 +1,26 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "ogldev_math_3d.h"
+
+class Camera
+{
+public:
+
+    Camera();
+
+    void SetPosition(float x, float y, float z);
+
+    void OnKeyboard(unsigned char key);
+
+    Matrix4f GetMatrix();
+
+private:
+
+    Vector3f m_pos;
+    Vector3f m_target;
+    Vector3f m_up;
+    float m_speed = 1.0f;
+};
+
+#endif  /* CAMERA_H */
